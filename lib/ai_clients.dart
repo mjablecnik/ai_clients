5908f12ba0178c1e6786838c9381553ff88139bd
Martin Jablečník <martin.jablecnik@email.cz>
2025-05-29 15:17:52 +0200

    feat: add AiClients class for instantiating AI clients

--- lib/ai_clients.dart
+++ lib/ai_clients.dart
@@ -1,5 +1,24 @@
 library;
 
+import 'clients/baseten_client.dart';
+import 'clients/openai_client.dart';
+import 'clients/together_client.dart';
+
+export 'clients/ai_client.dart';
 export 'clients/baseten_client.dart';
 export 'clients/openai_client.dart';
 export 'clients/together_client.dart';
+
+class AiClients {
+  static OpenAiClient openAi({String? apiUrl, String? apiKey}) {
+    return OpenAiClient(apiUrl: apiUrl, apiKey: apiKey);
+  }
+
+  static TogetherClient together({String? apiUrl, String? apiKey}) {
+    return TogetherClient(apiUrl: apiUrl, apiKey: apiKey);
+  }
+
+  static BasetenClient baseten({String? apiUrl, String? apiKey}) {
+    return BasetenClient(apiUrl: apiUrl, apiKey: apiKey);
+  }
+}
